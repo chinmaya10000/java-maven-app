1f9485ebd94d1d7e71a6864f82ac87f8127a4806
chinmaya <chinmaya@gmail.com>
2023-08-22 12:47:24 +0530

    add terraform remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "myapp-bucket"
+        key = "myapp/state.tfstate"
+        region = "us-east-2"
+    }
+}
+
+
 provider "aws" {
     region = var.region
 }
