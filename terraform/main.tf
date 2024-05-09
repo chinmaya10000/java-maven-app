3a7f3622ecffff334d6efbb642cb3d6d0906d379
chinmaya <chinmaya@gmail.com>
2024-05-09 15:16:29 +0530

    fix credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">=0.12"
+  backend "s3" {
+    bucket = "chinmaya2242"
+    key = "myapp/state.tfstate"
+    region = "us-east-2"
+  }
+}
+
+
 provider "aws" {
   region = var.region
 }
